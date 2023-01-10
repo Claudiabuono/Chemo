fea109ab162f1aa2effee558fa513d932b2ffa83
Gianpio Silvestri <Sceptile72@gmail.com>
2023-01-10 20:10:21 +0100

    Inserimento MedicineBean, StockBean e MedicineQueryBean

---
+++ src/main/java/medicinemanagement/storage/MedicineQueryBean.java
@@ -0,0 +1,110 @@
+package medicinemanagement.storage;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+import connector.DatabaseConnector;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.logging.Filter;
+
+public class MedicineQueryBean {
+
+    //Inserimento singolo documento nella Collection
+    public void insertDocument(MedicineBean medicine) {
+        //Recupera la Collection
+        MongoCollection<Document> collection = getCollection();
+
+        //Crea il documento da inserire nella Collection
+        Document document = createDocument(medicine);
+
+        //Inserisci il documento nella collection
+        collection.insertOne(document);
+
+        System.out.println("Documento inserito con successo nella Collection");
+    }
+
+    //Inserimento collezione di documenti nella Collection
+    public void insertDocuments(ArrayList<MedicineBean> medicines) {
+        //Recupera la Collection
+        MongoCollection<Document> collection = getCollection();
+
+        //Crea un documento per ogni medicinale in medicines
+        ArrayList<Document> docs = new ArrayList<>();
+        for(MedicineBean medicine : medicines) {
+            Document doc = createDocument(medicine);
+            docs.add(doc);
+        }
+
+        //Inserisci i documenti nella collection
+        collection.insertMany(docs);
+
+        System.out.println("Documenti inseriti con successo nella Collection");
+    }
+
+    //Elimina documento dalla Collection
+    public void deleteDocument(String key, String value) {
+        //Recupera la Collection
+        MongoCollection<Document> collection = getCollection();
+
+        //Crea il filtro
+        Bson filter = Filters.eq(key, value);
+
+        //Cancella il documento
+        collection.deleteOne(filter);
+
+        System.out.println("Documento eliminato con successo nella Collection");
+    }
+
+    //Modifica di un documento
+    public void updateDocument(String id, String valId, String key, String valKey) {
+        //Recupera la Collection
+        MongoCollection<Document> collection = getCollection();
+
+        //Crea il filtro
+        Bson filter = Filters.eq(id, valId);
+
+        //Aggiorna il documento
+        collection.updateOne(filter, Updates.set(key, valKey));
+
+        System.out.println("Documento aggiornato con successo nella Collection");
+    }
+
+    //Ricerca di un documento nella Collection data una coppia (key, value)
+    public Iterator<Document> findDocument(String key, String value) {
+        //Recupera la Collection
+        MongoCollection<Document> collection = getCollection();
+
+        //Crea il filtro
+        Bson filter = Filters.eq(key, value);
+
+        //Cerca il documento
+        FindIterable<Document> iterDoc = collection.find(filter);
+
+        return iterDoc.iterator();
+    }
+
+
+    //Metodi ausiliari
+    private MongoCollection<Document> getCollection() {
+        MongoDatabase mongoDatabase = DatabaseConnector.getDatabase();
+
+        MongoCollection<Document> collection = mongoDatabase.getCollection("medicinale");
+        System.out.println("Collection 'medicinale' recuperata con successo");
+        return collection;
+    }
+
+    private Document createDocument(MedicineBean medicine) {
+        return new Document("id", medicine.getId())
+                .append("nome", medicine.getNome())
+                .append("ingredienti", medicine.getIngredienti())
+                .append("quantita", medicine.getQuantita())
+                .append("lotto", medicine.getLotti());
+    }
+}
---
+++ src/main/java/medicinemanagement/storage/StockBean.java
@@ -0,0 +1,71 @@
+package medicinemanagement.storage;
+
+import java.util.Date;
+
+public class StockBean {
+
+    //Parametri
+    private boolean stato;
+    private Date scadenza;
+    private int capacita;
+    private String idLotto;
+
+    //Costruttori
+    public StockBean() {
+    }
+
+    public StockBean(boolean stato, Date scadenza, int capacita, String idLotto) {
+        this.stato = stato;
+        this.scadenza = scadenza;
+        this.capacita = capacita;
+        this.idLotto = idLotto;
+    }
+
+    //Getters
+
+    public boolean getStato() {
+        return stato;
+    }
+
+    public Date getScadenza() {
+        return scadenza;
+    }
+
+    public int getCapacita() {
+        return capacita;
+    }
+
+    public String getIdLotto() {
+        return idLotto;
+    }
+
+
+    //Setters
+    public void setStato(boolean stato) {
+        this.stato = stato;
+    }
+
+    public void setScadenza(Date scadenza) {
+        this.scadenza = scadenza;
+    }
+
+    public void setCapacita(int capacita) {
+        this.capacita = capacita;
+    }
+
+    public void setIdLotto(String idLotto) {
+        this.idLotto = idLotto;
+    }
+
+    //Metodi ereditati da Object
+
+    @Override
+    public String toString() {
+        return "StockBean{" +
+                "stato=" + stato +
+                ", scadenza=" + scadenza +
+                ", capacita=" + capacita +
+                ", idLotto='" + idLotto + '\'' +
+                '}';
+    }
+}
