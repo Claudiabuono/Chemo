69e76884f0bf85424a09dd2da852da890714cae2
alfoCaiazza <ACaiazza2000@gmail.com>
2023-01-10 16:10:01 +0100

    Inserimento bean, queryBean e nuovo package Inserimentio plannerBean, plannerQueryBean. Nel plannerBean è presente l'utilizzo della classe appointmentBean, rappresentante l'entità appuntamento

---
+++ src/main/java/plannerManagement/storage/AppointmentBean.java
@@ -0,0 +1,50 @@
+package plannerManagement.storage;
+
+import java.util.Date;
+
+public class AppointmentBean {
+    private String idPatient;
+    private Date date;
+    private String chair;
+
+    public AppointmentBean() {}
+
+    public AppointmentBean(String idPatient, Date date, String chair) {
+        this.idPatient = idPatient;
+        this.date = date;
+        this.chair = chair;
+    }
+
+    public String getIdPatient() {
+        return idPatient;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getChair() {
+        return chair;
+    }
+
+    public void setIdPatient(String idPatient) {
+        this.idPatient = idPatient;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setChair(String chair) {
+        this.chair = chair;
+    }
+
+    @Override
+    public String toString() {
+        return "AppointmentBean{" +
+                "idPatient='" + idPatient + '\'' +
+                ", date=" + date +
+                ", chair='" + chair + '\'' +
+                '}';
+    }
+}
