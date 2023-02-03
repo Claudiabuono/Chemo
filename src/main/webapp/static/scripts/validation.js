fefadfd765511aeb695b477845e5194279fdb850
Antonio Marcone <40888021+amarcone42@users.noreply.github.com>
2023-02-03 13:32:11 +0100

    Fix funzioni di validazione Modificato validation.js per correggere un errore nel controllo dei campi.

--- src/main/webapp/static/scripts/validation.js
+++ src/main/webapp/static/scripts/validation.js
@@ -1,12 +1,12 @@
 /* FUNZIONI PER IL CONTROLLO DEGLI INPUT GENERICI */
 function numberValidity(input) {
     var number = /^[0-9]+$/;
-    return input.value.match(number);
+    return input.match(number);
 }
 
 function nameValidity(input) {
     var name = /^[A-Za-z][A-Za-z\'\-]+([\ A-Za-z][A-Za-z\'\-]+)*$/;
-    return input.value.match(name);
+    return input.match(name);
 }
 
 
@@ -22,22 +22,22 @@ function cityValidity(input) {
 
 function dateValidity(input) {
     var date = /^(19|20)[0-9]{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/;
-    return input.value.match(date);
+    return input.match(date);
 }
 
 function taxCodeValidity(input) {
     var code = /^[A-Z]{6}[0-9]{2}[A-Z][0-9]{2}[A-Z][0-9]{3}[A-Z]$/i;
-    return input.value.match(code);
+    return input.match(code);
 }
 
 function phoneNumberValidity(input) {
     var phoneNumber = /^[\+]?[(]?[0-9]{2,3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,7}$/;
-    return input.value.match(phoneNumber);
+    return input.match(phoneNumber);
 }
 
 function notesValidity(input) {
     var notes = /^[A-Za-z0-9][A-Za-z0-9\'\-\.\,\n]+([\ A-Za-z0-9][A-Za-z0-9\'\-\.\,\n]+)*$/;
-    return input.value.match(notes);
+    return input.match(notes);
 }
 
 
@@ -45,12 +45,12 @@ function notesValidity(input) {
 
 function conditionValidity(input) {
     var condition = /^[A-Za-z0-9][A-Za-z0-9\'\-]+([\ A-Za-z0-9][A-Za-z0-9\'\-]+)*$/;
-    return input.value.match(condition);
+    return input.match(condition);
 }
 
 function idValidity(input) {
     var id = /^$/;          //Aggiungere regex id medicinale
-    return input.value.match(id);
+    return input.match(id);
 }
 
 function doseValidity(input) {
@@ -74,12 +74,12 @@ function frequencyValidity(input) {
 
 function medicineNameValidity(input) {
     var medicineName = /^[A-Za-z0-9][A-Za-z0-9\'\-]+([\ A-Za-z0-9][A-Za-z0-9\'\-]+)*$/;
-    return input.value.match(medicineName);
+    return input.match(medicineName);
 }
 
 function ingredientsValidity(input) {
     var ingredients = /^[A-Za-z0-9][A-Za-z0-9\'\-\.\,\n]+([\ A-Za-z0-9][A-Za-z0-9\'\-\.\,\n]+)*$/;
-    return input.value.match(ingredients);
+    return input.match(ingredients);
 }
 
 function quantityValidity(input) {
